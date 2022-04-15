32c55aaa01de7db3c929f12d2097f23268110519
nathalia <nathaliatroina@gmail.com>
2022-04-15 10:55:26 -0400

    background and moon growing in the home page

--- client/src/components/home/Home.js
+++ client/src/components/home/Home.js
@@ -2,7 +2,9 @@ import React, { useState, useEffect } from 'react';
 //import axios from 'axios';
 //import XMLParser from 'react-xml-parser';
 import { useHistory } from 'react-router-dom';
-import oceano from"./oceano.jpeg"
+import oceano from "./oceano.jpeg"
+import './Home.css'
+import apollomoon from "./apollomoon.png";
 
 function Home( { latitude, longitude, search, setResultData, searchTitle, setSearchTitle, searchUrl, setLatitude, setLongitude, shortenDecimals, setSearchResults, setNumbersOfDays, resultData, setSearch, makeCoordinates, numbersOfDays, options} ) {
     // history deixa voce navegar com clicks, deixa voce contolar o futuro
@@ -30,11 +32,14 @@ function Home( { latitude, longitude, search, setResultData, searchTitle, setSea
     return(
         <div className="mainhome" > 
         {/* <img className="oceanimage" src={oceano} alt="oceano" /> */}
+        <div className="backgroundimage" ></div>
+        <img className="apollomoon" src={apollomoon} alt="moon" />
         <form onSubmit={handleSubmit}>
             <input type="text" 
             onChange={handleSearchChange}
             />
         </form>
+
         </div>
     )
 }
