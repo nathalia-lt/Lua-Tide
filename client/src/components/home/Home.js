a241ffde0719d882b4f3c2479c6544a5f3cfc518
nathalia <nathaliatroina@gmail.com>
2022-04-18 14:28:44 -0400

    moon phases and rectangles

--- client/src/components/home/Home.js
+++ client/src/components/home/Home.js
@@ -79,49 +79,63 @@ function Home({ latitude, longitude, search, setResultData, searchTitle, setSear
                             <div></div>
                         </div>
                         <div className="rectangle">
-                        <img className="minimoon" src={tidecreatures} alt="creatures" />
+                            <img className="minimoon" src={tidecreatures} alt="creatures" />
                             <div className='rectangle-title'>5 ways animals use tides</div>
-                            <ul className= "tidecreatures">
+                            <ul className="tidecreatures">
                                 <li className="creatures"> <b>Crabs:</b> Burrowing into the sand</li>
-                                <li className="creatures"> <b>Seaweed and Sea-squirts:</b> Being covered with thick slime</li>   
-                                <li className="creatures"> <b>Snails:</b> Moving with the falling tide</li>  
-                                <li className="creatures"> <b>Limpet:</b> Clamping down onto a rock</li>         
-                                <li className="creatures"> <b>Mussels and Barnacles:</b> shutting their shells tight</li> 
-                                </ul>
+                                <li className="creatures"> <b>Seaweed and Sea-squirts:</b> Being covered with thick slime</li>
+                                <li className="creatures"> <b>Snails:</b> Moving with the falling tide</li>
+                                <li className="creatures"> <b>Limpet:</b> Clamping down onto a rock</li>
+                                <li className="creatures"> <b>Mussels and Barnacles:</b> shutting their shells tight</li>
+                            </ul>
                         </div>
-                        
+
                     </div>
                     <div className="moonphases">
-                            <div className='rectangle-moon'>Moon Phases</div>
-                            <ul className= "moonphaseslist">
+                        <div className='rectangle-moon'>Moon Phases</div>
+                        <div className='moonphases-information'>
+                            <ul className="moonphaseslist">
                                 {/* quando eu tenho somente o texto, posso colocar so com o elemento li,
                                 mas como eu quero acresecentar tambem uma imagem tenho que colocar div */}
-                                <li className="phases"> 
+                                <li className="phases">
                                     <div><b>New Moon:</b> When the Earth, Moon and Sun align, the gravitational attraction exerted by the two stars on the oceans adds up, generating sea currents that cause a maximum rise in sea level.</div>
-                                     <div className='moonimgwrapper'>
-                                         <img className="moonpic" src={newmoonremove} alt="New Moon" />
-                                         </div> 
-                                     </li>
+
+                                </li>
                                 <li className="phases"> <div><b>Waxing Moon:</b> In this phase the Moon and the Sun form a right angle. Thus, lunar gravitation opposes solar. Even so, the level differences between high and low tides are much smaller.</div>
-                                 <div className='moonimgwrapper'>
-                                     <img className="moonpic" src={waxingcrescentremove} alt="Waxing Moon" /> 
-                                     <img className="moonpic" src={waxinggibousremove} alt="Waxing Moon" /> 
-                                     </div>
-                                     </li>
+                                </li>
                                 <li className="phases"> <div><b>Full Moon:</b> About two weeks after the Nova phase, the Moon assumes a position where it lines up with the Sun and Earth. This combination brings a new wave of high tides.</div>
-                                <div className='moonimgwrapper'>
-                                    <img className="moonpic" src={fullmoonremove} alt="Full Moon" />
-                                    </div>
-                                    </li>  
+                                </li>
                                 <li className="phases"> <div><b>Waning Moon:</b> In this lunar phase, there is a decrease in the influence of the Sun and Moon on the tides. On the night when half the Moon is visible, the attraction reaches its lowest value.</div>
-                                <div className='moonimgwrapper'>
-                                    <img className="moonpic" src={waningcrescentremove} alt="Waning Moon" /> 
-                                    <img className="moonpic" src={waninggibousremove} alt="Waning Moon" />
+                                </li>
+
+                            </ul>
+                            <ul className='moonimages-list'>
+                                <li className='phases-images'>
+                                    <div className='moonimgwrapper'>
+                                        <img className="moonpic" src={newmoonremove} alt="New Moon" />
                                     </div>
-                                    </li>   
-        
-                                </ul>
+                                </li>
+                                <li className='phases-images'>
+                                    <div className='moonimgwrapper'>
+                                        <img className="moonpic" src={waxingcrescentremove} alt="Waxing Moon" />
+                                        <img className="moonpic" src={waxinggibousremove} alt="Waxing Moon" />
+                                    </div>
+                                </li>
+                                <li className='phases-images'>
+                                    <div className='moonimgwrapper'>
+                                        <img className="moonpic" src={fullmoonremove} alt="Full Moon" />
+                                    </div>
+                                </li>
+                                <li className='phases-images'>
+                                    <div className='moonimgwrapper'>
+                                        <img className="moonpic" src={waningcrescentremove} alt="Waning Moon" />
+                                        <img className="moonpic" src={waninggibousremove} alt="Waning Moon" />
+                                    </div>
+                                </li>
+
+                            </ul>
                         </div>
+                    </div>
                 </div>
             </div>
         </React.Fragment>
