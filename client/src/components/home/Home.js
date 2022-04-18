b6769ea8499720f160d2e048c6341eedfdbe8cd3
nathalia <nathaliatroina@gmail.com>
2022-04-18 13:23:52 -0400

    add moon phases images

--- client/src/components/home/Home.js
+++ client/src/components/home/Home.js
@@ -7,10 +7,15 @@ import './Home.css'
 import apollomoon from "./apollomoon.png";
 import arrowdown from "./arrowdown.png";
 import { animateScroll as ScrollAction } from 'react-scroll';
-import moonphases from "./moonphases.jpeg";
 import fishhunter from './fishhunter.jpeg'
 import tidecreatures from './tidecreatures.jpeg'
 import tide from './tide.jpeg'
+import newmoonremove from './newmoonremove.png'
+import fullmoonremove from './fullmoonremove.png'
+import waningcrescentremove from './waningcrescentremove.png'
+import waninggibousremove from './waninggibousremove.png'
+import waxingcrescentremove from './waxingcrescentremove.png'
+import waxinggibousremove from './waxinggibousremove.png'
 
 function Home({ latitude, longitude, search, setResultData, searchTitle, setSearchTitle, searchUrl, setLatitude, setLongitude, shortenDecimals, setSearchResults, setNumbersOfDays, resultData, setSearch, makeCoordinates, numbersOfDays, options }) {
     // history deixa voce navegar com clicks, deixa voce contolar o futuro
@@ -70,14 +75,14 @@ function Home({ latitude, longitude, search, setResultData, searchTitle, setSear
                         </div>
                         <div className="rectangle">
                             <img className="minimoon" src={tide} alt="Tide" />
-                            <div className='rectangle-title'>Moon phases</div>
+                            <div className='rectangle-title'>Extremes</div>
                             <div></div>
                         </div>
                         <div className="rectangle">
                         <img className="minimoon" src={tidecreatures} alt="creatures" />
                             <div className='rectangle-title'>5 ways animals use tides</div>
                             <ul className= "tidecreatures">
-                                <li className="creatures" > <b>Crabs:</b> Burrowing into the sand</li>
+                                <li className="creatures"> <b>Crabs:</b> Burrowing into the sand</li>
                                 <li className="creatures"> <b>Seaweed and Sea-squirts:</b> Being covered with thick slime</li>   
                                 <li className="creatures"> <b>Snails:</b> Moving with the falling tide</li>  
                                 <li className="creatures"> <b>Limpet:</b> Clamping down onto a rock</li>         
@@ -87,13 +92,34 @@ function Home({ latitude, longitude, search, setResultData, searchTitle, setSear
                         
                     </div>
                     <div className="moonphases">
-                        <img className="moonphasesimage" src={moonphases} alt="moonphases" />
                             <div className='rectangle-moon'>Moon Phases</div>
                             <ul className= "moonphaseslist">
-                                <li className="phases" > <b>New Moon:</b> When the Earth, Moon and Sun align, the gravitational attraction exerted by the two stars on the oceans adds up, generating sea currents that cause a maximum rise in sea level.</li>
-                                <li className="phases"> <b>Waning Moon:</b> In this lunar phase, there is a decrease in the influence of the Sun and Moon on the tides. On the night when half the Moon is visible, the attraction reaches its lowest value.</li>   
-                                <li className="phases"> <b>Full Moon:</b> About two weeks after the Nova phase, the Moon assumes a position where it lines up with the Sun and Earth. This combination brings a new wave of high tides.</li>  
-                                <li className="phases"> <b> Crescent Moon:</b> In this phase the Moon and the Sun form a right angle. Thus, lunar gravitation opposes solar. Even so, the level differences between high and low tides are much smaller.</li>         
+                                {/* quando eu tenho somente o texto, posso colocar so com o elemento li,
+                                mas como eu quero acresecentar tambem uma imagem tenho que colocar div */}
+                                <li className="phases"> 
+                                    <div><b>New Moon:</b> When the Earth, Moon and Sun align, the gravitational attraction exerted by the two stars on the oceans adds up, generating sea currents that cause a maximum rise in sea level.</div>
+                                     <div className='moonimgwrapper'>
+                                         <img className="moonpic" src={newmoonremove} alt="New Moon" />
+                                         </div> 
+                                     </li>
+                                <li className="phases"> <div><b>Waxing Moon:</b> In this phase the Moon and the Sun form a right angle. Thus, lunar gravitation opposes solar. Even so, the level differences between high and low tides are much smaller.</div>
+                                 <div className='moonimgwrapper'>
+                                     <img className="moonpic" src={waxingcrescentremove} alt="Waxing Moon" /> 
+                                     <img className="moonpic" src={waxinggibousremove} alt="Waxing Moon" /> 
+                                     </div>
+                                     </li>
+                                <li className="phases"> <div><b>Full Moon:</b> About two weeks after the Nova phase, the Moon assumes a position where it lines up with the Sun and Earth. This combination brings a new wave of high tides.</div>
+                                <div className='moonimgwrapper'>
+                                    <img className="moonpic" src={fullmoonremove} alt="Full Moon" />
+                                    </div>
+                                    </li>  
+                                <li className="phases"> <div><b>Waning Moon:</b> In this lunar phase, there is a decrease in the influence of the Sun and Moon on the tides. On the night when half the Moon is visible, the attraction reaches its lowest value.</div>
+                                <div className='moonimgwrapper'>
+                                    <img className="moonpic" src={waningcrescentremove} alt="Waning Moon" /> 
+                                    <img className="moonpic" src={waninggibousremove} alt="Waning Moon" />
+                                    </div>
+                                    </li>   
+        
                                 </ul>
                         </div>
                 </div>
